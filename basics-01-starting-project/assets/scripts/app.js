f981b8fcf64f646ad84abaa66beaa5f699c7e9ac
Niidarku <niidarku66@gmail.com>
2020-11-04 12:30:23 +0000

    progress on objects and arrays

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,21 +1,79 @@
-
-
 const defaultValue = 0;
 let currentResult = defaultValue;
+logEntries = [];
 
+
+function getValue(){
+  return parseInt(userInput.value)
+}
+
+function getDescription(operation, valueBefore, finalValue){
+  const resultDescription= `${valueBefore} ${operation} ${finalValue}`
+  outputResult(currentResult, resultDescription)
+}
+
+//shorthand to log Into console
+function log(x){
+  return console.log(x)
+}
+
+//to get the logEntry
+function writeLog(operation, 
+  beforeAdded, 
+  afterAdd){
+    const logEntry = {
+      operation: operation,
+      beforeAdded: beforeAdded,
+      afterAdd: afterAdd
+    };
+    logEntries.push(logEntry);
+    log(logEntries)
+}
 
 function add(){
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, '')
+  const enetedNumber = getValue()
+  const initalValue = currentResult;
+  currentResult += enetedNumber;
+  getDescription("+", initalValue, enetedNumber)
+  writeLog('Add', initalValue, currentResult)
 }
 
 
+function subtraction(){
+  const enetedNumber = getValue()
+  const initalValue = currentResult;
+  currentResult -= enetedNumber;
+  getDescription('-', initalValue, enetedNumber);
+  writeLog('Subtraction', initalValue, currentResult)
+}
 
-function fullName(firstname, surname){
-  return firstname 
+function mutiply(){
+  const enetedNumber = getValue()
+  const initalValue = currentResult;
+  currentResult = currentResult * enetedNumber;
+  getDescription('*', initalValue, enetedNumber)
+  writeLog('Multiplied', initalValue, currentResult)
+}
+
+function divides(){
+  const enetedNumber = getValue()
+  const initalValue = currentResult;
+  currentResult = currentResult / enetedNumber;
+  getDescription('/', initalValue, enetedNumber)
+  writeLog('Divided', initalValue, currentResult)
 }
 
 addBtn.addEventListener('click', add)
+
+subtractBtn.addEventListener('click', subtraction)
+
+multiplyBtn.addEventListener('click', mutiply)
+
+divideBtn.addEventListener('click', divides)
+
+
+
+
 
 
 
