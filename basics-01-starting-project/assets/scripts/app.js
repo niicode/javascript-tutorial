a4250000c4b8a30815cdef81f1119cfb144ad609
Niidarku <niidarku66@gmail.com>
2020-11-04 01:07:05 +0000

    Javascript tutorials

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,45 @@
+
+
+const defaultValue = 0;
+let currentResult = defaultValue;
+
+
+function add(){
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '')
+}
+
+
+
+function fullName(firstname, surname){
+  return firstname 
+}
+
+addBtn.addEventListener('click', add)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
